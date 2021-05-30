8cec670cd14b037792d0e375bd3fe75901a201a6
Chen Liang <cliang@burning-glass.com>
2021-05-30 17:48:34 -0400

    multiple udpates
    mask the audio data folder
    placeholder for converter

--- listener.py
+++ listener.py
@@ -1,6 +1,8 @@
 import ffmpeg
 import queue
 import sys
+import os
+import datetime
 import sounddevice as sd
 import json
 import numpy as np
@@ -14,12 +16,18 @@ logging.basicConfig(
 
 class AudioInfo(object):
     def __init__(self, url, stream, channels, samplerate):
+        self.flag = url.split("/")[-1]
         self.url = url
         self.stream_codec_name = stream["codec_long_name"]
         self.channels = channels
         self.samplerate = samplerate
         self.stream = stream
 
+    def save(self, manifest_dir="./", manifest_name=None):
+        manifest_name = manifest_name or f"{self.flag}.json"
+        manifest_fpath = os.path.join(manifest_dir, manifest_name)
+        with open(manifest_fpath, mode="w") as f:
+            json.dump(self.__dict__, f, indent=2)
 class NumpyAudioFrame(object):
     def __init__(self, frame_vector, prec=4):
         self.frame_vector = frame_vector
@@ -75,51 +83,66 @@ def callback(outdata, frames, time, status):
     assert len(data) == len(outdata)
     outdata[:] = data
 
-def listener_main(url, device=None, blocksize=1024, buffersize=20, holder=5):
+def listener_main(url, device=None, blocksize=1024, buffersize=20, 
+                  retain=5, raw_audio_dir=None, manifest_dir=None):
+
+    raw_audio_dir = raw_audio_dir or "./raw_audio"
+    manifest_dir = manifest_dir or "./manifests"
+    os.makedirs(raw_audio_dir, exist_ok=True)
+    os.makedirs(manifest_dir, exist_ok=True)
 
     logging.info('Opening stream ...')
     global q
     q = queue.Queue(maxsize=buffersize)
     metainfo = fetch_audio_info(url)
+    metainfo.save(manifest_dir=manifest_dir)
 
     try:
-        process = ffmpeg.input(
+        ffmpeg_listen_process = ffmpeg.input(
             metainfo.url
         ).output(
-            'pipe:',
+            'pipe:.mp3',
             format='f32le', # float 32 bit input
             acodec='pcm_f32le',
             ac=metainfo.channels, # 1 channel only
             ar=metainfo.samplerate, # given sample rate
-            loglevel='quiet',
+            loglevel='panic',
         ).run_async(pipe_stdout=True)
         stream = sd.RawOutputStream(
             samplerate=metainfo.samplerate, blocksize=blocksize,
             device=device, channels=metainfo.channels, dtype='float32',
-            callback=callback)
+            callback=callback
+        )
         read_size = blocksize * metainfo.channels * stream.samplesize
         logging.info('Buffering ...')
         for _ in range(buffersize):
-            metablock = process.stdout.read(read_size)
+            metablock = ffmpeg_listen_process.stdout.read(read_size)
             q.put_nowait(metablock)
         logging.info('Starting Playback ...')
         with stream:
             timeout = blocksize * buffersize / metainfo.samplerate
-            holder_seq = b''
-            holder_cnt = -1
+            capture_seq = b''
+            retain_cnt = -1
             while True:
-                metablock = process.stdout.read(read_size)
+                metablock = ffmpeg_listen_process.stdout.read(read_size)
                 np_block = NumpyAudioFrame(np.frombuffer(metablock, dtype='float32'))
                 np_block_info = np_block.__repr__()
                 if np_block.retain == True:
-                    holder_cnt = holder
-                if holder_cnt >= 0:
-                    logging.info(f"Capture {np_block_info}, Serialize the next frame ({holder_cnt})")
-                    holder_seq += metablock
-                    holder_cnt -= 1
+                    retain_cnt = retain
+                if retain_cnt > 0:
+                    logging.info(f"Capture {np_block_info}, Fetch Next Frame ({retain_cnt})")
+                    capture_seq += metablock
+                    retain_cnt -= 1
+                elif retain_cnt == 0:
+                    time_str = datetime.datetime.now().strftime(format="%Y%m%d-%H%M%S")
+                    export_audio_path = os.path.join(raw_audio_dir, f"{metainfo.flag}-{time_str}.raw")
+                    logging.warning(f"Export raw file to path: {export_audio_path}")
+                    with open(export_audio_path, mode="wb") as f:
+                        f.write(capture_seq)
+                    retain_cnt -= 1
                 else:
-                    holder_seq = b''
-                    holder_cnt = -1
+                    capture_seq = b''
+                    retain_cnt = -1
                 q.put(metablock, timeout=timeout)
     except KeyboardInterrupt:
         logging.info('Terminated due to Interrupted by user')
@@ -134,12 +157,12 @@ if __name__ == "__main__":
 
     URL="http://d.liveatc.net/kbos_twr"
     DEVICE=2
-    BLOCKSIZE=1024
+    BLOCKSIZE=2048
     BUFFERSIZE=20
-    HOLDER=15
-    
+    RETAIN=5
+    EXPORT_DIR="./raw_audio"
 
-    listener_main(URL, DEVICE, BLOCKSIZE, BUFFERSIZE, HOLDER)
+    listener_main(URL, DEVICE, BLOCKSIZE, BUFFERSIZE, RETAIN, EXPORT_DIR)
 
     # >>> import sounddevice as sd
     # >>> sd.query_devices()
