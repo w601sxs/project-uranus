0dcdc1b2a0dce843424199714b25a759a4e8393a
Chen Liang <cliang@burning-glass.com>
2021-05-31 16:18:38 -0400

    update stream info

--- stream_finder.py
+++ stream_finder.py
@@ -1,6 +1,6 @@
 from lxml import etree
 import os
-import sys
+import datetime
 import pandas as pd
 import logging
 import urllib.request as urlrequest
@@ -36,17 +36,34 @@ def fetch_main(export_path=None):
         logging.info(f"Parsed URL: {url}")
         html = etree.HTML(page)
         available_href = [item.attrib["href"]
-            for item in html.xpath("//table[@bgcolor='#EEEEEE']//a[contains(@href, '/play/')]")
+            for item in html.xpath("//table[@bgcolor='#EEEEEE']//td[@bgcolor='lightgreen']//a[contains(@href, '/play/')]")
         ]
+        available_texts = [list(item.itertext()) 
+            for item in html.xpath("//table[@bgcolor='#EEEEEE']//td[@bgcolor='lightgreen']")
+        ]
+
         available_flags = [get_flag(pls_link) for pls_link in available_href]
         available_links = [flag_to_stream_link(flag) for flag in available_flags]
-        available_abstract = [item.text
-            for item in html.xpath("//table[@bgcolor='#EEEEEE']//a[contains(@onclick, 'myDirectStream')]")
-        ]
-        assert len(available_links) == len(available_abstract)
+        available_abstract = [texts[0] for texts in available_texts]
+        available_loc = [texts[2] for texts in available_texts]
+        available_metar = [texts[11] for texts in available_texts]
+        assert len(available_links) == len(available_texts)
         logging.info(f"Fetched {len(available_links)} Links through URL: {url}")
-        stream_info_df = [{"flag": flag, "stream_link": url, "abstract": abstract, "category": category, "metar": None, "description": None} for flag, url, abstract in zip(available_flags, available_links, available_abstract)]
-        stream_info_df = pd.DataFrame.from_records(stream_info_df).set_index("flag")
+        stream_info_df = [
+            {"flag": flag, "stream_link": url, "abstract": abstract, "category": category, "metar": metar, "location": loc}
+            for flag, url, abstract, metar, loc in zip(available_flags, available_links, available_abstract, available_metar, available_loc)
+        ]
+        stream_info_df = pd.DataFrame.from_records(stream_info_df)
+        stream_info_df["fetch-time"] = datetime.datetime.utcnow().strftime(format="%Y-%m-%d %H:%M:%S UTC")
+
+        # post process of metar to assure the metar information is correct
+        stream_info_df.loc[
+            stream_info_df["metar"].map(lambda x: x[:4].lower()) != stream_info_df["flag"].map(lambda x: x[:4].lower()),
+            "metar"
+        ] = None
+
+        stream_info_df = stream_info_df.set_index("flag")
+
         stream_df_collection[category] = stream_info_df
 
     stream_final_df = pd.concat([meta_df for meta_df in stream_df_collection.values()])
