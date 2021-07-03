ffe7a51df33af87c46906977bd9af115cc041f06
Chen Liang <cliang@burning-glass.com>
2021-07-03 16:57:43 -0400

    simplify the convert and listen process

---
+++ listener.py
@@ -0,0 +1,82 @@
+import os
+import datetime
+import subprocess
+import json
+
+class AudioStream(object):
+    def __init__(self, url, flag, probe):
+
+        self.url = url
+        self.flag = flag
+        self.probe = probe
+        self.stream = None
+        self.codec_type = None
+        self.channels = None
+        self.samplerate = None
+
+        streams = self.probe.get("streams", [])
+        assert len(streams) == 1, \
+            'Ternimated due to: there must be exactly one stream available'
+        
+        self.stream = streams[0]
+        self.codec_type = self.stream.get("codec_type", None)
+        assert self.codec_type == 'audio', \
+            'Ternimated due to: The stream must be an audio stream'
+        self.channels = self.stream.get("channels", None)
+        self.samplerate = self.stream.get('sample_rate', None)
+        self.describe()
+
+    def describe(self):
+        display_info = {
+            "target_url": self.url,
+            "flag": self.flag,
+            "codec_type": self.codec_type,
+            "channels": self.channels,
+            "samplerate": self.samplerate
+        }
+        print(f"Acquired Stream Info: {display_info}")
+        return 
+        
+    def save_manifest_file(self, manifest_dir):
+        manifest_fpath = os.path.join(manifest_dir, f"{self.flag}.json")
+        print(f"Save Manifest File to {manifest_fpath}")
+        os.makedirs(os.path.dirname(manifest_fpath), exist_ok=True)
+        with open(manifest_fpath, mode="w") as f:
+            json.dump(self.__dict__, f, indent=2)
+
+def deploy_listener_main(url, probe, flag, data_dir, runtime):
+
+    manifest_dir = os.path.join(data_dir, "manifests")
+    raw_audio_dir = os.path.join(data_dir, "raw_audio")
+
+    print(f'Deploy ffmpeg probe on URL: {url}')
+    audio_stream = AudioStream(url, flag, probe)
+    audio_stream.save_manifest_file(manifest_dir)
+    current_time = datetime.datetime.now()
+    duration = datetime.timedelta(seconds=runtime)
+    end_time = current_time + duration
+    start_time_stamp = current_time.strftime("%Y%m%d%H%M%S%f")[:-3]
+    end_time_stamp = end_time.strftime("%Y%m%d%H%M%S%f")[:-3]
+    duration_time_stamp = str(duration)
+    raw_audio_path = os.path.join(
+    raw_audio_dir, 
+        f"{audio_stream.flag}-{start_time_stamp}-{end_time_stamp}.mp3"
+    )
+    cmd = " ".join(
+        [
+            "ffmpeg", 
+            "-i", str(audio_stream.url),
+            "-t", str(duration_time_stamp),
+            "-ac", str(audio_stream.channels),
+            "-ar", str(audio_stream.samplerate),
+            "-loglevel", "error",
+            raw_audio_path
+        ]
+    )
+    try:
+        print(f"... Execute {cmd}")
+        subprocess.check_output(cmd, shell=True)
+    except Exception as e:
+        print(f"[{audio_stream.flag}] Terminated due to exception {e}")
+    finally:
+        return
--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,11 @@
 from metaflow import FlowSpec, step, Parameter
 from stream_finder import crawl_stream_info
 from initiate import setup
+from listener import deploy_listener_main
+from converter import deploy_converter_main
 import os
 import json
+import ffmpeg
 
 class AudioCrawlingFLow(FlowSpec):
 
@@ -16,17 +19,36 @@ class AudioCrawlingFLow(FlowSpec):
         help="the json file path to export the stream info generated from step find_streams",
         default="data/stream_info/stream_info.json"
     )
+    listener_runtime = Parameter(
+        "listener_runtime",
+        help="runtime of a listener",
+        default=3600
+    )
+    min_silence_len = Parameter(
+        "min_silence_len",
+        help="silence interval in one audio segment (ms)",
+        default=500
+    )
+    silence_thresh = Parameter(
+        "silence_thresh",
+        help="threshold of volume in audio segment we treat as silence status (DB)",
+        default=-40
+    )
+    extend = Parameter(
+        "extend",
+        help="time that we extend each audio segment (ms)",
+        default=10
+    )
     dry_run = Parameter(
         "dry_run",
         help="dry run flag, if True, remove the existing data directory when run the pipeline",
         default=False
     )
-    safe_mode = Parameter(
-        "safe_mode",
-        help="safe_mode_falg, if True if limits the number of listeners to be 3",
+    back_door = Parameter(
+        "back_door",
+        help="back_door_flag, if True if we'll skip some of the process or limit the deployed resources",
         default=False
     )
-
 
     @step
     def start(self):
@@ -38,26 +60,75 @@ class AudioCrawlingFLow(FlowSpec):
     def find_streams(self):
         print("Entering stream searching")
         print(f"expected export the stream info path to {os.path.abspath(self.stream_info_path)}")
-        if self.dry_run is True or not os.path.exists(self.stream_info_path):
+        if not self.back_door is True or not os.path.exists(self.stream_info_path):
             self.stream_info_records = crawl_stream_info(self.stream_info_path)
         else:
             print("Found existing stream info in data directory, skipped")
             with open(self.stream_info_path, mode="r") as f:
                 self.stream_info_records = json.load(f)
-        if self.safe_mode is True:
-            self.stream_info_records = self.stream_info_records[:3]
-        self.next(self.dispatch_listener, foreach="stream_info_records")
+        self.next(self.deploy_listerners)
 
     @step
-    def dispatch_listener(self):
-        self.listener_info = self.input
-        print("Listener targeting on URL: {}".format(self.listener_info["stream_link"]))
-        
-        self.next(self.join)
+    def deploy_listerners(self):
+        if self.back_door is True:
+            self.use_agents = self.stream_info_records[:3]
+        else:
+            self.use_agents = self.stream_info_records
+        if self.dry_run is True:
+            cmd = "rm -f " + os.path.join(self.data_dir, "manifests/*")
+            os.system(cmd)
+            cmd = "rm -f " + os.path.join(self.data_dir, "raw_audio/*")
+            os.system(cmd)
+        self.next(self.deploy_listener, foreach="use_agents")
 
     @step
-    def join(self, inputs):
+    def deploy_converter(self):
+        if self.dry_run is True:
+            cmd = "rm -f " + os.path.join(self.data_dir, "processed_audio/*")
+            os.system(cmd)
+        deploy_converter_main(
+            self.data_dir
+        )
         self.next(self.end)
+        
+
+    @step
+    def deploy_listener(self):
+        self.listener_info = self.input
+        self.url = self.listener_info["stream_link"]
+        self.flag = self.url.split("/")[-1]
+        print(f"[{self.flag}] Listener targeting on URL: {self.url}")
+        if self.back_door == True:
+            listener_runtime = 120
+        else:
+            listener_runtime = self.listener_runtime
+        try:
+            self.probe = ffmpeg.probe(
+                self.url, 
+                loglevel="error"
+            )
+        except ffmpeg.Error as e:
+            print(f"[{self.flag}] Terminate due to exception: {e.stderr.decode().strip()}")
+        except Exception as e:
+            print(f"[{self.flag}] Terminate due to exception: {e}")
+        else:
+            print(f"[{self.flag}] Captured probe, proceed")
+            deploy_listener_main(
+                url = self.url,
+                flag = self.flag,
+                probe = self.probe, 
+                data_dir = self.data_dir,
+                runtime = listener_runtime,
+            )
+        self.next(self.join_listeners)
+
+    @step
+    def join_listeners(self, inputs):
+        self.next(self.deploy_converter)
+
+    # @step
+    # def join_mainflow(self, inputs):
+    #     self.next(self.end)
 
     @step
     def end(self):
